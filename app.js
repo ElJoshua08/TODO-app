b195ed2260071c41184c63f7eafe5e9b7b102806
Joshua <josuealejandrof926@gmail.com>
2023-08-17 02:52:38 +0200

    complete list creation and start element creation

--- app.js
+++ app.js
@@ -1,89 +1,50 @@
-const list_items = document.getElementById("list-container");
-const add_item = document.getElementById("item-add");
+// Elements
+const list_template = document.querySelector("#list_template").content;
+const list_clone = document.importNode(list_template, true);
 
-const body = document.querySelector("body");
+const item_template = document.querySelector("#item_template");
+const item_clone = document.importNode(item_template.content, true);
 
-add_item.addEventListener("click", addItem);
+const add_list_btn = document.querySelector(".add-list");
+const adding_container = document.querySelector(".adding");
+const typing_container = document.querySelector(".typing");
 
-console.log(document);
+// Variables
+const lists = [];
 
-function addItem() {
-  let item = document.createElement("li");
-  let input = document.createElement("input");
-  input.placeholder = "Write new task here";
-  let text = document.createElement("h1");
-  let btns = document.createElement("div");
-  let check = document.createElement("i");
-  let uncheck = document.createElement("i");
-  let end_write = document.createElement("i");
-
-  item.classList.add("item", "task_write");
-
-  input.classList.add("task_input");
-
-  end_write.classList.add("fa-solid", "fa-check", "btn", "end_task__btn");
-
-  check.classList.add("fa-solid", "fa-check");
-  check.id = "done";
-
-  check.addEventListener("click", checkFunc);
-  uncheck.addEventListener("click", deleteItemFunc);
-
-  uncheck.classList.add("fa-solid", "fa-trash");
-  uncheck.id = "undone";
-
-  btns.appendChild(check);
-  btns.appendChild(uncheck);
-
-  btns.classList.add("btns");
-
-  if (item.classList.contains("task_write")) {
-    item.appendChild(input);
-    item.appendChild(end_write);
-
-    end_write.onclick = (e) => {
-      item.classList.remove("task_write");
-      text.innerHTML = input.value;
-      item.appendChild(text);
-      item.appendChild(btns);
-
-      if (text.scrollWidth > item.clientWidth - 115) {
-        text.classList.add("scroll");
-      }
-
-      item.removeChild(input);
-      item.removeChild(end_write);
-    };
-  }
-
-  list_items.appendChild(item);
+// Functions
+function getListContent(list) {
+  let list_content = list.firstElementChild.children[1];
+  return list_content;
 }
 
-function checkFunc() {
-  let item = this.parentElement.parentElement;
-  item.classList.add("complete");
+add_list_btn.addEventListener("click", function () {
+  adding_container.classList.remove("active");
+  typing_container.classList.add("active");
+
+  typing_container.lastElementChild.addEventListener("click", addNewList);
+});
+
+function addNewList() {
+  const newList = list_clone.firstElementChild.cloneNode(true);
+
+  newList.lastElementChild.addEventListener("click", function() {
+    addNewItem(newList)
+  })
+
+  newList.firstElementChild.children[0].innerHTML =
+  typing_container.firstElementChild.value;
+
+  add_list_btn.classList.remove("active");
+  document.body.appendChild(newList);
+  lists.push(newList);
 }
 
-function deleteItemFunc() {
-  let item = this.parentElement.parentElement;
-  let items_container = item.parentElement;
+function addNewItem(list) {
+  const newItem = item_clone.firstElementChild.cloneNode(true)
+  const list_content = list.firstElementChild.children[1]
 
-  if (item.classList.contains("complete")) {
-    item.classList.remove("complete");
-  } else {
-    items_container.removeChild(item);
-  }
-}
+  list_content.appendChild(newItem)
 
-// Toggle theme
-let theme_state = 0;
-
-function changeTheme() {
-  if (theme_state == 0) {
-    body.classList.replace("light_theme", "dark_theme");
-    theme_state = 1;
-  } else {
-    body.classList.replace("dark_theme", "light_theme");
-    theme_state = 0;
-  }
+  console.log(list_content)
 }
